68587e43780c46d554999f42b4e1c19cdaba2558
Daniel <edbs08@outlook.com>
2021-09-01 12:36:41 -0500

    all_test_passed

--- main.py
+++ main.py
@@ -15,10 +15,6 @@ class Geometry():
     def __init__(self):
         pass
     
-    # def distance_calculation(self,coordinate1,coordinate2):
-    #     dist = math.sqrt((coordinate1.x-coordinate2.x) ** 2+(coordinate1.y-coordinate2.y) ** 2)
-    #     return dist
-    
     def distance_calculation(self,automaton1,automaton2):
         dist = math.sqrt((automaton2.coordinate.x-automaton1.coordinate.x) ** 2+(automaton2.coordinate.y-automaton1.coordinate.y) ** 2)
         return dist
@@ -78,10 +74,6 @@ while True:
         zombies.append(zombie)
 
     
-    # Get distances
-    # zombies to humans
-    dist_z_h = []
-    
     closest_zombie_list =[]
     human_to_save_list = []
     zombies_ids = []
@@ -89,7 +81,6 @@ while True:
         min_dist = 18000
         for zombie in zombies:
             for human in humans:
-                #dist = geometry.distance_calculation(zombie.coordinate,human.coordinate)
 
                 if zombie.id_zombie not in zombies_ids:
                     dist = geometry.distance_calculation(zombie,human)
@@ -98,31 +89,17 @@ while True:
                         closest_zombie = zombie
                         human_to_save = human
         closest_zombie_list.append(closest_zombie)
-        zombies_ids.append(zombie.id_zombie)
+        zombies_ids.append(closest_zombie.id_zombie)
         human_to_save_list.append(human_to_save)
-        # print("Min distance in humanid {} and zombieid {}".format(human.id_human,zombie.id_zombie), file=sys.stderr, flush=True)
-        # array ordered by distance                
 
-    # print("Human coordinates 1... {} {}".format(human_to_save_list[0].coordinate.x,human_to_save_list[0].coordinate.y), file=sys.stderr, flush=True)
-    # print("Zombies coordinates 1... {} {}".format(closest_zombie_list[0].coordinate.x,closest_zombie_list[0].coordinate.y), file=sys.stderr, flush=True)
-    # check if Ash reaches
-    # dist_1 = geometry.distance_calculation(hunter,human_to_save_list[0])
-    # print("Hunter to humans... {}".format(dist_1), file=sys.stderr, flush=True)
-    # dist_2 = geometry.distance_calculation(closest_zombie_list[0],human_to_save_list[0])
-    # print("Zombie to humans... {}".format(dist_2), file=sys.stderr, flush=True)
-    
-    #for array ordered by distance
-    #condition
+    #iterate throguh possible human-zombie 
     for ii in range(len(human_to_save_list)):
         human= human_to_save_list[ii]
         zombie= closest_zombie_list[ii]
         dist_1 = geometry.distance_calculation(hunter,human)
         dist_2 = geometry.distance_calculation(zombie,human)
-        print("Zombies coordinates ... {} {}".format(zombie.coordinate.x,zombie.coordinate.y), file=sys.stderr, flush=True)
-        print("Human coordinates ... {} {}".format(human.coordinate.x,human.coordinate.y), file=sys.stderr, flush=True)
-        print("dist 1  {}".format((dist_1-2000)/1000), file=sys.stderr, flush=True)
-        print("dist_2. {}".format((dist_2+400)/400), file=sys.stderr, flush=True)
  
+        # condition to check if hunter ash can save at least one (will break if not)
         if float((dist_1-2000)/1000) < float((dist_2+400)/400):
             #it can save human
             midpoint = [round((human.coordinate.x+zombie.coordinate.x)/2),round((human.coordinate.y+zombie.coordinate.y)/2)]
@@ -135,9 +112,3 @@ while True:
     centinela += 1
     zombies.clear()
     humans.clear()
-
-
-    
-
-
-    
